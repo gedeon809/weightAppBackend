dfe410382611ef50b3a6a6bcdd997b2bcf4618fc
Gedeon Kakona <gedeonkakona@gmail.com>
2023-02-28 23:45:57 +0200

    update weight routes

--- routes/weight.js
+++ routes/weight.js
@@ -4,11 +4,11 @@ import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 //create weight
-router.post("/addWeight", verifyToken, addWeight);
+router.post("/addWeight", addWeight);
 //get weight
-router.get("/getWeight", verifyToken, getWeight);
+router.get("/getWeight", getWeight);
 //delete weight
-router.delete("/:id", verifyToken, deleteWeight);
+router.delete("/:id", deleteWeight);
 //update weight
 router.put("/:id", verifyToken, updateWeight);
 
